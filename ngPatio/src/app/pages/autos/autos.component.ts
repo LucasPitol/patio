4fafc7d26313d22b7b87548246c2768c2d37d99e
Lucas Pitol <lucascpitol0@gmil.com>
2019-04-25 19:07:57 -0300

    card body

--- ngPatio/src/app/pages/autos/autos.component.ts
+++ ngPatio/src/app/pages/autos/autos.component.ts
@@ -19,7 +19,8 @@ export class AutosComponent implements OnInit {
 	loading = true;
 
 	cards = [
-		{"id": 1, "imageUrl": '', "marca": "Volkswagen", "modelo": "Golf", "versao": "2.0 GTI", "cambio": "Automático"}
+		{"id": 1, "imageUrl": 'assets/golf.jpg', "marca": "Volkswagen", "modelo": "Golf", "versao": "2.0 GTI 4P", "cambio": "Automático", "valor": 138.41},
+		{"id": 2, "imageUrl": 'https://abrilquatrorodas.files.wordpress.com/2018/02/ford-ka_plus-2019-1600-06.jpg?quality=70&strip=info&strip=info', "marca": "Ford", "modelo": "KA +", "versao": "1.5 Sigma 4P", "cambio": "Manual", "valor": 55.67}
 	];
 	
 	ngOnInit() {
