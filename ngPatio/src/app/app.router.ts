5ee8e4ed36f2dbd5ebee6d101e685907d3885667
Lucas Pitol <lucascpitol0@gmil.com>
2019-04-29 17:28:27 -0300

    Abre tela do anuncio / amber purple

--- ngPatio/src/app/app.router.ts
+++ ngPatio/src/app/app.router.ts
@@ -2,12 +2,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { AutosComponent } from './pages/autos/autos.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AnunciarComponent } from './pages/anunciar/anunciar.component';
+import { AnuncioComponent } from './pages/anuncio/anuncio.component';
 
 const appRoutes: Routes = [
     { path: 'autos', component: AutosComponent},
     { path: 'home', component: HomeComponent },
-    { path: 'anunciar', component: AnunciarComponent}
-    // { path: 'noticia/:idcard', component: NoticiaComponent }
+    { path: 'anunciar', component: AnunciarComponent},
+    { path: 'anuncio/:idanuncio', component: AnuncioComponent }
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);
---
+++ ngPatio/src/app/pages/anuncio/anuncio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'anuncio-component',
+    templateUrl: './anuncio.component.html',
+    styleUrls: ['./anuncio.component.css']
+})
+
+export class AnuncioComponent {
+
+    loading = true;
+
+}
