88e14c01d29351268e5f9d2832886f13934c8f90
Lucas Pitol <lucascpitol0@gmil.com>
2019-06-06 12:07:14 -0300

    Remove mocação e loga erro da requisição

--- ngPatio/src/app/pages/autos/autos.component.ts
+++ ngPatio/src/app/pages/autos/autos.component.ts
@@ -52,13 +52,6 @@ export class AutosComponent implements OnInit {
 	date = new FormControl(new Date());
 	serializedDate = new FormControl((new Date()).toISOString());
 
-	// cards = [
-	// 	{"id": 1, "imageUrl": 'assets/golf.jpg', "marca": "Volkswagen", "modelo": "Golf", "versao": "2.0 GTI 4P", "cambio": "Automático", "valor": 168.41},
-	// 	{"id": 2, "imageUrl": 'https://abrilquatrorodas.files.wordpress.com/2018/02/ford-ka_plus-2019-1600-06.jpg?quality=70&strip=info&strip=info', "marca": "Ford", "modelo": "KA +", "versao": "1.5 Sigma 4P", "cambio": "Manual", "valor": 55.67},
-	// 	{"id": 3, "imageUrl": 'https://www.cstatic-images.com/car-pictures/xl/usc70toc041g021001.png', "marca": "Toyota", "modelo": "Corolla", "versao": "2.0 XRS 4P", "cambio": "Automático", "valor": 132.51},
-	// 	{"id": 4, "imageUrl": 'https://catalogo.webmotors.com.br/imagens/prod/347912/AUDI_A3_1.4_TFSI_FLEX_SEDAN_PRESTIGE_TIPTRONIC_34791212005833228.png?s=fill&w=440&h=330&q=80&t=true', "marca": "Audi", "modelo": "A3", "versao": "2.0 TFSI 4P", "cambio": "Automático", "valor": 132.51},
-	// ];
-
 	ngOnInit() {
 
 		this.filterservice.applyFilter.subscribe(
@@ -77,10 +70,14 @@ export class AutosComponent implements OnInit {
 
 	getCardsForHome()
 	{
-		this.db.list('/cars').valueChanges().subscribe(cars => {
-			this.cards = cars;
-			console.log(this.cards)
-			this.loading = false;
-		});
+		this.db.list('/cars').valueChanges().subscribe(
+			cars => {
+				this.cards = cars;
+				this.loading = false;
+			},
+			error => {
+				console.log(error);
+			}
+		);
 	}
 }
